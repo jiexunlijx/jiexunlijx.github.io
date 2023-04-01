2156703affc5c7b0f03bed6997bbfddc2dd8de00
jiexunlijx <129481730+jiexunlijx@users.noreply.github.com>
2023-04-01 09:49:58 +0800

    Update DallE_Test.py

--- DallE_Test.py
+++ DallE_Test.py
@@ -4,11 +4,12 @@ import openai
 openai.api_key = "Your API key"
 
 while True:
-    a = input("Enter a prompt or "quit" to exit: ")
-    if a == "exit" or a == "quit":
+    req = input("Enter a prompt or 'quit' to exit: ")
+    if req == "exit" or req == "quit":
         break
     response = openai.Image.create(
-    prompt= a,
+    prompt= req,
+    #change 'n' to request more images from the API (max 10)
     n=1,
     #change image size for lower cost. 512x512, 256x256
     size="1024x1024"
